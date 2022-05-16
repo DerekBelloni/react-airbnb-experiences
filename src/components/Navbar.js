f0f20ccc4a7e25a69e7c31470cd986cd85ffcc40
Derek <derekbelloni10@gmail.com>
2022-05-16 09:39:36 -0600

    Navbar logo rendering

---
+++ src/components/Navbar.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import airLogo from '../images/airbnb-logo.png'
+
+const Navbar = () => {
+  return (
+    <img className="navbar-img" src={airLogo} alt="" />
+  )
+}
+
+export default Navbar;
