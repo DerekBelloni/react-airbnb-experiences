20a740cd0d2d27d514d8d1aa4bfc34ab40373ded
Derek <derekbelloni10@gmail.com>
2022-05-17 11:10:28 -0600

    Data component created

--- src/App.js
+++ src/App.js
@@ -2,13 +2,22 @@ import React from 'react';
 import Navbar from "./components/Navbar";
 import Hero from './components/Hero';
 import Card from "./components/Card";
+import swimmer from './images/swimmer.png'
+
 
 const App = () => {
   return (
     <>
       <Navbar />
       <Hero />
-      <Card />
+      <Card
+        img={swimmer}
+        rating="5.0"
+        reviewCount="(6)"
+        country="Italy"
+        title="Life Lesson with Katie Zaferes"
+        price={136}
+      />
     </>
   )
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -2,18 +2,22 @@ import React from 'react';
 import swimmerImg from '../images/swimmer.png';
 import star from '../images/red-star.png';
 
-const Card = () => {
+const Card = (props) => {
+  console.log(props)
   return (
     <div className="card--body">
-      <img className="card--img" src={swimmerImg} alt="" />
-      <div>
+      <img className="card--img" src={props.img} alt="" />
+      <div className="card--stats">
         <img className="card--star" src={star} alt="" />
-        <p>5.0 (6)-USA</p>
-        <p>Life lessons with Katie Zefares</p>
-        <p><b>From $136</b>/person</p>
+        <span>{props.rating} </span>
+        <span className="grey">{props.reviewCount} - </span>
+        <span className="grey">{props.country}</span>
       </div>
+      <p>{props.title}</p>
+      <p><span className="bold">From ${props.price}</span>/person</p>
     </div>
   )
 }
+
 
 export default Card;
